f02c12224913f2c29f48d38c517490d6a2f76a1c
Salihcan Kahya <salihcankahya@gmail.com>
2020-12-07 16:15:31 +0300

    Initial Commit

---
+++ Kelimelerim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kelimelerim
+//
+//  Created by Salihcan Kahya on 7.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kelimelerim/KelimelerimApp.swift
@@ -0,0 +1,17 @@
+//
+//  KelimelerimApp.swift
+//  Kelimelerim
+//
+//  Created by Salihcan Kahya on 7.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct KelimelerimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
